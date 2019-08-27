f16476ac92d38aeb8603ee9d1ab8e52599063716
Fernando <fernando.muramasa@gmail.com>
2019-08-27 16:44:04 -0300

    Teste no ToTitleCase na classe TextHelperTests

--- TutorialEcommerce.Helpers.Tests/TextHelperTests.cs
+++ TutorialEcommerce.Helpers.Tests/TextHelperTests.cs
@@ -12,5 +12,29 @@ namespace TutorialEcommerce.Helpers.Tests
 
             Assert.Equal("123", textoLimpo);
         }
+
+        [Fact]
+        public void ToTitleCase_Tudo_Maiusculo()
+        {
+            var texto = TextHelper.ToTitleCase("GOD OF WAR");
+
+            Assert.Equal("God Of War", texto);
+        }
+
+        [Fact]
+        public void ToTitleCase_Tudo_Minusculo()
+        {
+            var texto = TextHelper.ToTitleCase("god of war");
+
+            Assert.Equal("God Of War", texto);
+        }
+
+        [Fact]
+        public void ToTitleCase_Aleatorio()
+        {
+            var texto = TextHelper.ToTitleCase("gOd oF WaR");
+
+            Assert.Equal("God Of War", texto);
+        }
     }
 }
