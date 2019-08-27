7afe467f2a1b2b957451659a569fe5b4df4cb58f
Fernando <fernando.muramasa@gmail.com>
2019-08-27 09:56:21 -0300

    Ajustes

---
+++ TutorialEcommerce.Helpers/Guard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialEcommerce.Helpers
+{
+    public class Guard
+    {
+        public void ForNullOrEmpty(string value , string msgError)
+        {
+
+        }
+    }
+}
