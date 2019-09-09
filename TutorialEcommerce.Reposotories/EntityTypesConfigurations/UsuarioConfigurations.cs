f7912aa2e2e57174c276240d5c7ebf0060549ec5
Fernando <fernando.muramasa@gmail.com>
2019-09-09 13:11:40 -0300

    Adicionado EntityFramework e casse UsuarioConfigurations

---
+++ TutorialEcommerce.Reposotories/EntityTypesConfigurations/UsuarioConfigurations.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using TutorialEcommerce.Domain.Entities;
+
+namespace TutorialEcommerce.Reposotories.EntityTypesConfigurations
+{
+    public class UsuarioConfigurations : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioConfigurations()
+        {
+            Property(x => x.Cpf.Codigo)
+                .HasColumnName("Cpf")
+                .HasColumnAnnotation(IndexAnnotation.AnnotationName, new IndexAttribute("IX_CPF", 1) { IsUnique = true });
+
+            Property(x => x.Login)
+                .HasMaxLength(Usuario.LoginMaxValue)
+                .HasColumnAnnotation(IndexAnnotation.AnnotationName,
+                new IndexAnnotation(new IndexAttribute("IX_Login", 2) { IsUnique = true }));
+        }
+    }
+}
